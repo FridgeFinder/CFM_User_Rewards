e48471a8d39f698b92d7f535992e6480b3abcb63
Sneha <snehamur@uw.edu>
2026-03-20 13:22:03 -0700

    refined app.py method

--- functions/fridge_report_consumer/app.py
+++ functions/fridge_report_consumer/app.py
@@ -47,20 +47,19 @@ def _process_event(event: dict[str, Any], request_id: str) -> dict:
     user_id = detail.get("userId", "<null>")
     new_report = detail.get("newReport", "<null>")
     previous_report = detail.get("previousReport", "<null>")
-    if if not user_id:
+    if not user_id:
         return {"skipped": True}
 
 # 1. convert the string to json
-    json_user_id = json.loads(user_id)
+    # json_user_id = json.loads(user_id)
     json_new_report = json.loads(new_report)
     json_previous_report = json.loads(previous_report)
 
-    if (json_new_report != json_previous_report) {
+    if (json_new_report == json_previous_report):
         return {"skipped": True}
-    }
+    
 # 2. generate the award id using AWARD#STATUS_UPDATE#FRIDGE#{fridgeId}#TS#{timestamp}
-    award_id = f"AWARD#STATUS_UPDATE#FRIDGE#{json_new_report[fridge_id]}#TS#{json_new_report[epochTimestamp]}"
-
+    award_id = f"AWARD#STATUS_UPDATE#FRIDGE#{json_new_report['fridgeId']}#TS#{json_new_report['epochTimestamp']}"
     # get the list of ACTION TYPES using the  (rules.py) function, get fridge report awards
     awards: List[ACTION_TYPES] = get_fridge_report_awards(new_report, previous_report)
 
